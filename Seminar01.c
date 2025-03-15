6b12f10582be29ed973796f6ba8fd0b7d4a00ca0
Vlad <vladv@DESKTOP-SBC1GS8>
2025-03-15 15:10:49 +0200

    Citirea unui numar intreg

--- Seminar01.c
+++ Seminar01.c
@@ -2,6 +2,9 @@
 
 
 int main() {
-	printf("Salutare!\n");
+	printf("Salutare!\n Introdu un numar intreg: ");
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus %d:", variabila);
 	return 0;
 }
